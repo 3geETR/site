15f2474a569859274898b21e4a1e5aa306577c97
3geE <egemacun@gmail.com>
2024-07-24 21:53:56 +0300

    Initial commit

---
+++ blog.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('blog.json')
+        .then(response => response.json())
+        .then(data => {
+            const blogPostsContainer = document.getElementById('blog-posts');
+            data.forEach((post, index) => {
+                const postElement = document.createElement('div');
+                postElement.classList.add('blog-post');
+
+                const titleElement = document.createElement('h2');
+                const linkElement = document.createElement('a');
+                linkElement.href = `blog-detail.html?post=${index}`;
+                linkElement.textContent = post.title;
+                titleElement.appendChild(linkElement);
+
+                const dateElement = document.createElement('p');
+                dateElement.textContent = post.date;
+                dateElement.classList.add('blog-date');
+
+                const contentElement = document.createElement('p');
+                const shortContent = post.content.length > 100 ? post.content.substring(0, 100) + '...' : post.content;
+                contentElement.innerHTML = shortContent; // HTML olarak içerik ekleniyor
+                contentElement.classList.add('blog-content');
+
+                const readMoreElement = document.createElement('a');
+                readMoreElement.href = `blog-detail.html?post=${index}`;
+                readMoreElement.textContent = 'Read more';
+                readMoreElement.classList.add('read-more');
+
+                postElement.appendChild(titleElement);
+                postElement.appendChild(dateElement);
+                postElement.appendChild(contentElement);
+                postElement.appendChild(readMoreElement);
+
+                blogPostsContainer.appendChild(postElement);
+            });
+        })
+        .catch(error => console.error('Error fetching blog posts:', error));
+});
